4a553b68f849372228bb4fea99431d77dc2a0e91
PedemonteGiacomo <giacomopedemonte@libero.it>
2023-04-15 19:13:49 +0200

    other tryes with asyncio

--- sample_cmdi_test.py
+++ sample_cmdi_test.py
@@ -55,8 +55,8 @@ test_pages = {
     'ping.php': test_values_ping,
 }
 
-@asyncio.coroutine
-def test_step(test_name, test_value, url):
+
+async def test_step(test_name, test_value, url, loop):
     cookies = {'a': '1'}
     headers = {}
     params = {
@@ -65,13 +65,23 @@ def test_step(test_name, test_value, url):
         'input': test_value,
     }
 
-    #print(f"\nTest performed on {url}")
-    future = loop.run_in_executor(None, requests.get(url, params, cookies, headers))
-    response = yield from future1
-    
+    complete_url = url+'/?host='+test_value
+    response = await loop.run_in_executor(None, requests.get, complete_url)
     test_result = WHOAMI_ORACLE not in response.text
-
     print("{} {}".format("\u2705" if test_result else "\u274c", test_name))
+
+async def main():
+    for page, test_values in test_pages.items():
+        print(f"\nRunning tests for {page}\n")
+        url = SERVER_URL + '/' + page
+        tasks = []
+        for test_name, test_value in test_values.items():
+            task = asyncio.create_task(test_step(test_name, test_value, url, loop))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
 
 # this for sequential running
 #for page, test_values in test_pages.items():
@@ -80,28 +90,3 @@ def test_step(test_name, test_value, url):
 #    for test_name, test_value in test_values.items():
 #        test_step(test_name, test_value, url)
 
-async def main():
-    tasks = []
-    loop = asyncio.get_event_loop()
-    for page, test_values in test_pages.items():
-        print(f"\nRunning tests for {page}\n")
-        url = SERVER_URL + '/' + page
-        for test_name, test_value in test_values.items():
-            task = asyncio.create_task(test_step(test_name, test_value, url))
-            tasks.append(task)
-        await asyncio.gather(*tasks)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-    
-#async def main():
-#    async with asyncio.get_event_loop() as loop:
-#        tasks = []
-#        for page, test_values in test_pages.items():
-#            url = SERVER_URL + '/' + page
-#            print(f"\nTest performed on {url}")
-#            for test_name, test_value in test_values.items():
-#                task = asyncio.create_task(test_step(test_name, test_value, url))
-#                tasks.append(task)
-#            await asyncio.gather(*tasks)
-#
