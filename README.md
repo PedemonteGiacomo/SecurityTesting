3de1bfd2cf4b0982b166d60ddd159b030a68a9e2
PedemonteGiacomo <giacomopedemonte@libero.it>
2023-04-15 18:32:19 +0200

    tryes with async

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # SecurityTesting
-This repo will contain all the works related to Security Testing
+In this assignment the main goal is to obtain succesfull injections to understand how works Security testing in order to obtain a full vision of possible attacks caused by a bad manage of the code.
 
-# TODO
-1. Make a good report of all those injections tests
+So, in this directory are contained several pages that are our target to obtain specific injections reported in the sample_cmdi_test.py.
+
+The test are obtained using the requests module to make command injections, argument injections and blind injections.
+
+The output of the execution of that file report with a green check if the tests passes in the sense that we handle and manage that particular injection, otherwise a red check in the other case.
+
+Tests are performed on each different pages contained in this directory in order to obtain a full vision of the attacked performed.
+
+
+
+In the attached version is reported a version of the pages in order to avoid the injections and be able to pass all the tests.
+
+
--- sample_cmdi_test.py
+++ sample_cmdi_test.py
@@ -1,3 +1,4 @@
+import asyncio
 import requests
 
 WHOAMI_ORACLE = "giacomo"
@@ -12,9 +13,6 @@ test_values_echo = {
     'pipe': '|whoami',
     'subshell': '$(whoami)',
     'backticks': '`whoami`',
-    # blind injections
-    'boolean-based blind': 'if [ -f ping.php ]; then whoami; fi', 
-    'boolean-based blind 2': '; if [ -f {} ]; then whoami; fi'.format(FILE_EXISTS_ORACLE),
     'blind injection redirection': '; whoami > whoami_echo.txt',
 }
 
@@ -57,6 +55,7 @@ test_pages = {
     'ping.php': test_values_ping,
 }
 
+@asyncio.coroutine
 def test_step(test_name, test_value, url):
     cookies = {'a': '1'}
     headers = {}
@@ -66,14 +65,43 @@ def test_step(test_name, test_value, url):
         'input': test_value,
     }
 
-    response = requests.get(url, params=params, cookies=cookies, headers=headers)
-
+    #print(f"\nTest performed on {url}")
+    future = loop.run_in_executor(None, requests.get(url, params, cookies, headers))
+    response = yield from future1
+    
     test_result = WHOAMI_ORACLE not in response.text
 
     print("{} {}".format("\u2705" if test_result else "\u274c", test_name))
 
-for page, test_values in test_pages.items():
-    url = SERVER_URL + '/' + page
-    print(f"\nTest performed on {url}")
-    for test_name, test_value in test_values.items():
-        test_step(test_name, test_value, url)
+# this for sequential running
+#for page, test_values in test_pages.items():
+#    url = SERVER_URL + '/' + page
+#    print(f"\nTest performed on {url}")
+#    for test_name, test_value in test_values.items():
+#        test_step(test_name, test_value, url)
+
+async def main():
+    tasks = []
+    loop = asyncio.get_event_loop()
+    for page, test_values in test_pages.items():
+        print(f"\nRunning tests for {page}\n")
+        url = SERVER_URL + '/' + page
+        for test_name, test_value in test_values.items():
+            task = asyncio.create_task(test_step(test_name, test_value, url))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+    
+#async def main():
+#    async with asyncio.get_event_loop() as loop:
+#        tasks = []
+#        for page, test_values in test_pages.items():
+#            url = SERVER_URL + '/' + page
+#            print(f"\nTest performed on {url}")
+#            for test_name, test_value in test_values.items():
+#                task = asyncio.create_task(test_step(test_name, test_value, url))
+#                tasks.append(task)
+#            await asyncio.gather(*tasks)
+#
